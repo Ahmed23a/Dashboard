466e0900629e24cf8defad1650706b142eaac579
dalialotfy <dalia.momen99@eng-st.cu.edu.eg>
2023-05-02 01:34:23 +0300

    MUI Dashboard example added ..

---
+++ Dalia/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Dashboard from './Components/Dashboard/Dashboard';
+import './index.css';
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Dashboard></Dashboard>
+  </React.StrictMode>
+);
+
